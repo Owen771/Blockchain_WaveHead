ade6bea05a70fb78720d7ad9fae3f96ef849b6be
OwenMac <owen4123@gmail.com>
2022-02-21 18:39:44 +0800

    init

---
+++ scripts/deploy.js
@@ -0,0 +1,41 @@
+// deploy.js: used to deploy the smart contract to blockchain
+//            after deployed, this script will return an addr, take that addr and
+//            /artifacts/contracts/WavePortal.sol/WavePortal.json as ABI
+//            update to the web app
+
+const main = async () => {
+  // const [deployer] = await hre.ethers.getSigners();
+  // const accountBalance = await deployer.getBalance();
+
+  // console.log("Deploying contracts with account: ", deployer.address);
+  // console.log("Account balance: ", accountBalance.toString());
+
+  // const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  // const waveContract = await waveContractFactory.deploy({
+  //   value: hre.ethers.utils.parseEther("0.001"),
+  // });
+  // await waveContract.deployed();
+
+  // console.log("WavePortal address: ", waveContract.address);
+
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
